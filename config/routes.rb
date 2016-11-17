380a4af78c4938c21f6a8f69940480f1fbdc5c6e
cjohnston9 <coltsfan17111@gmail.com>
2016-11-16 22:41:56 -0500

    Finished chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -57,5 +58,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-	root 'application#hello'
 end
