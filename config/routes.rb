71401879e47d761f4bc36d71b1156b4ca5a077de
cjohnston9 <coltsfan17111@gmail.com>
2016-11-17 07:24:03 -0500

    Chapter completed but static_pages has several errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'  # The priority is based upon order of creation: first created -> highest priority.
+  get  '/signup',  to: 'users#new'
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
